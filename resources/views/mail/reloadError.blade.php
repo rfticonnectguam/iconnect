10c2d68880359f82c7ae6bfc7dcb8a24f38dd07c
Reygie Tarroquin <reygie@Coffee2Codes-MacBook-Pro.local>
2018-04-10 10:04:01 +0800

    modified email template

--- resources/views/mail/reloadError.blade.php
+++ resources/views/mail/reloadError.blade.php
@@ -3,9 +3,11 @@
 <head>
 	<title></title>
 </head>
-<body>
-	<p>Good day {{ $name }},</p>
+<body style="text-align: center;"><br/><br/>
+	<img src="//i.imgur.com/htRzs4M_d.jpg?maxwidth=640&shape=thumb&fidelity=medium">
+	<p>Good day <strong>{{ $name }}</strong>,</p>
 	<p>The card that you selected are not available at this time.</p>
-	<p>Your load will be send to you later. Thank you.</p>
+	<p>Your PIN and Serial Number will be send to you later. Thank you.</p>
+
 </body>
 </html>
