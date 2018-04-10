f51b62f8921efdf39be5ea3953fa633ef3b670c3
Reygie Tarroquin <reygie@Coffee2Codes-MacBook-Pro.local>
2018-04-10 10:10:53 +0800

    modified factories of all products

--- database/factories/Lte_3_dayFactory.php
+++ database/factories/Lte_3_dayFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Lte_3_day::class, function (Faker $faker) {
     return [
         //
     	'serial_number' => mt_rand(100000, 999999),
-        'pin' => str_random(10),
+        'pin' => strtoupper(str_random(10)),
         'availability' => '0',
     ];
 });
--- database/factories/Prepaid_5Factory.php
+++ database/factories/Prepaid_5Factory.php
@@ -6,7 +6,7 @@ $factory->define(App\Prepaid_5::class, function (Faker $faker) {
     return [
    	     //
     	'serial_number' => mt_rand(100000, 999999),
-        'pin' => str_random(10),
+        'pin' => strtoupper(str_random(10)),
         'availability' => '0',
     ];
 });
