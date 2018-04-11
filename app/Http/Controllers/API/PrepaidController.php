d64258df4efe7f16939dbf8254626e64a1abe4f4
Reygie Tarroquin <reygie@Coffee2Codes-MacBook-Pro.local>
2018-04-11 08:29:55 +0800

    removed unused functions, include xcript validation library

--- app/Http/Controllers/API/PrepaidController.php
+++ app/Http/Controllers/API/PrepaidController.php
@@ -19,10 +19,11 @@ use DateTime;
 use Mail; //added mail function
 use App\Mail\sendPaymentErrorEmail;//added payment error email
 
+use App\Library\xcriptValidation;//added xcript custom validation library
+
 class PrepaidController extends Controller
 {
     //
-    
     public function getNumberOfAvailablePrepaid5(){
 
 
@@ -266,6 +267,8 @@ class PrepaidController extends Controller
         $ErrorPayload = (object) [];
         $ErrorStatus = false;
 
+        $xcript = new xcriptValidation;//include xcriptValidation
+
         //return 'show ok';
         $validation = $this->validatePrepaidPayment($request->all());
 
@@ -281,24 +284,18 @@ class PrepaidController extends Controller
                 $ErrorStatus = true;
             }
 
-            //check if email is valid 
-            //RFC EMAIL REGEX
-            $pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
-
-            if (!preg_match($pattern, $request->Email)) {
-                // emailaddress is invalid
-                 $ErrorPayload->Email = ["Invalid Email"];
-                 $ErrorStatus = true;
+            if(!$xcript->validateEmail($request->Email)){
+                $ErrorPayload->Email = ["Invalid Email"];
+                $ErrorStatus = true;
             }
+           
             
             //check if valid credit card number
-            $validate =  $this->isValidCC($request->CCNumber);
-
-            if(!$validate){
+            if(!$xcript->validateCC($request->CCNumber)){
                 $ErrorPayload->CCNumber = ['Invalid Credit card number'];
                 $ErrorStatus = true;
             }
-
+            
             if(!$ErrorStatus){
 
                 //valid data
@@ -423,33 +420,7 @@ class PrepaidController extends Controller
         ];
        
     }
-
-
-    public function isValidCC($cc_number) {
-        // Strip any non-digits (useful for credit card numbers with spaces and hyphens)
-        $cc_number = preg_replace('/\D/', '', $cc_number);
-        // Set the string length and parity
-        $number_length = strlen($cc_number);
-        $parity = $number_length % 2;
-        // Loop through each digit and do the maths
-        $total = 0;
-        for ($i = 0; $i < $number_length; $i++) {
-            $digit = $cc_number[$i];
-            // Multiply alternate digits by two
-            if ($i % 2 == $parity) {
-                $digit *= 2;
-                // If the sum is two digits, add them together (in effect)
-                if ($digit > 9) {
-                    $digit -= 9;
-                }
-            }
-            // Total up the digits
-            $total += $digit;
-        }
-        // If the total mod 10 equals 0, the number is valid
-        return ($total % 10 == 0) ? true : false;
-    }
-
+    
     public function getAvailableCard(){
 
         $card = [
