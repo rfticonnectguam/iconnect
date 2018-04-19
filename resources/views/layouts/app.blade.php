c866d76495aa493fdfd942937bd19ebe187f3598
reygie <reygie.tarroquin@choicephonellc.com>
2018-04-19 03:08:55 +0000

    hot fixed on layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
 <div class="sticky-top">
     <nav class="navbar navbar-expand-lg navbar-light bg-light middleNav">
       <div class="container">
-        <a href="/">
+        <a href="{{url('/')}}">
           <img class="HeaderLogo" src="{{ url('images/iconnect-logo.png') }}" >
         </a>
          {{--  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
